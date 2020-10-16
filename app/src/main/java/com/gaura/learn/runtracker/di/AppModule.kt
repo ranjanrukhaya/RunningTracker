8d946d6cfaa2cc21207c6c0ebd3860b77e33ec22
Ranjan Rukhaya <Ranjan.Rukhaya@gmail.com>
2020-10-16 07:56:17 +0530

    Dagger Hilt Setup

--- app/src/main/java/com/gaura/learn/runtracker/di/AppModule.kt
+++ app/src/main/java/com/gaura/learn/runtracker/di/AppModule.kt
@@ -1,7 +1,31 @@
 package com.gaura.learn.runtracker.di
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.gaura.learn.runtracker.db.RunDAO
+import com.gaura.learn.runtracker.db.RunningDatabase
+import com.gaura.learn.runtracker.other.Constants.RUNNING_DATABASE_NAME
 import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 @Module
+@InstallIn(ApplicationComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(@ApplicationContext app: Context) = Room.databaseBuilder(
+        app,
+        RoomDatabase::class.java,
+        RUNNING_DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRunDao(db: RunningDatabase) = db.getRunDao()
 }
