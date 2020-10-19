29a94d3d0b0c896ed48d6122d5255dd619a9c556
Ranjan Rukhaya <Ranjan.Rukhaya@gmail.com>
2020-10-19 11:11:30 +0530

    Handling Location Permissions through EasyPermissions library

--- app/src/main/java/com/gaura/learn/runtracker/other/Constants.kt
+++ app/src/main/java/com/gaura/learn/runtracker/other/Constants.kt
@@ -3,4 +3,5 @@ package com.gaura.learn.runtracker.other
 object Constants {
 
     const val RUNNING_DATABASE_NAME = "running_db"
+    const val REQUEST_CODE_LOCATION_PERMISSION = 1
 }
--- app/src/main/java/com/gaura/learn/runtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/gaura/learn/runtracker/ui/fragments/RunFragment.kt
@@ -1,17 +1,23 @@
 package com.gaura.learn.runtracker.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.gaura.learn.runtracker.R
+import com.gaura.learn.runtracker.other.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.gaura.learn.runtracker.other.TrackingUtility
 import com.gaura.learn.runtracker.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -19,8 +25,55 @@ class RunFragment : Fragment(R.layout.fragment_run) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requestPermissions()
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+    }
+
+    private fun requestPermissions() {
+        if (TrackingUtility.hasLocationPermissions(requireContext())) {
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "Please accept location permission to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "Please accept location permission to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        // Do nothing
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 }
