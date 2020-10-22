bdb9e75a41a69836067841f2a46e77867a3e354d
Ranjan Rukhaya <Ranjan.Rukhaya@gmail.com>
2020-10-22 16:43:00 +0530

    Making Our Service a Foreground Service

--- app/src/main/java/com/gaura/learn/runtracker/ui/MainActivity.kt
+++ app/src/main/java/com/gaura/learn/runtracker/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.gaura.learn.runtracker.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.gaura.learn.runtracker.R
+import com.gaura.learn.runtracker.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,7 +18,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //setSupportActionBar(toolbar)
+        navigateToTrackingFragmentIfNeeded(intent)
+
+        setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
 
         navHostFragment.findNavController()
@@ -27,5 +31,16 @@ class MainActivity : AppCompatActivity() {
                     else -> bottomNavigationView.visibility = View.GONE
                 }
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
