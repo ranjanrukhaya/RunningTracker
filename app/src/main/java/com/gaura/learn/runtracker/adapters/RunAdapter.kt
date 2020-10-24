294b61b80a22694a587acef4862c0256b8c73168
Ranjan Rukhaya <Ranjan.Rukhaya@gmail.com>
2020-10-24 16:32:08 +0530

    Showing Runs in the RecyclerView

---
+++ app/src/main/java/com/gaura/learn/runtracker/adapters/RunAdapter.kt
@@ -0,0 +1,72 @@
+package com.gaura.learn.runtracker.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.gaura.learn.runtracker.R
+import com.gaura.learn.runtracker.db.Run
+import com.gaura.learn.runtracker.other.TrackingUtility
+import kotlinx.android.synthetic.main.item_run.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class RunAdapter : RecyclerView.Adapter<RunAdapter.RunViewHolder>() {
+
+    inner class RunViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    val diffCallback = object : DiffUtil.ItemCallback<Run>() {
+        override fun areItemsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+    val differ = AsyncListDiffer(this, diffCallback)
+
+    fun submitList(list: List<Run>) {
+        differ.submitList(list)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
+        return RunViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_run, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
+        val run = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(run.img).into(ivRunImage)
+            val calendar = Calendar.getInstance().apply {
+                timeInMillis = run.timestamp
+            }
+
+            val dateFormat = SimpleDateFormat("dd.MM.yy", Locale.getDefault())
+            tvDate.text = dateFormat.format(calendar.time)
+
+            val avgSpeed = "${run.avgSpeedInKMH}km/h"
+            tvAvgSpeed.text = avgSpeed
+
+            val distance = "${run.distanceInMeters / 1000f}km"
+            tvDistance.text = distance
+
+            tvTime.text = TrackingUtility.getFormattedStopWatchTime(run.timeInMillis)
+
+            val cal = "${run.caloriesBurned} kcal"
+            tvCalories.text = cal
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+}
