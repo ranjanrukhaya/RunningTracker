cafc3b414177f373f8cafa75041d34dd2735c4dc
Ranjan Rukhaya <Ranjan.Rukhaya@gmail.com>
2020-10-24 19:35:03 +0530

    Saving Username and Weight

--- app/src/main/java/com/gaura/learn/runtracker/di/AppModule.kt
+++ app/src/main/java/com/gaura/learn/runtracker/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.gaura.learn.runtracker.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.gaura.learn.runtracker.db.RunningDatabase
+import com.gaura.learn.runtracker.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.gaura.learn.runtracker.other.Constants.KEY_NAME
+import com.gaura.learn.runtracker.other.Constants.KEY_WEIGHT
 import com.gaura.learn.runtracker.other.Constants.RUNNING_DATABASE_NAME
+import com.gaura.learn.runtracker.other.Constants.SHARED_PREF_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,6 +35,26 @@ object AppModule {
     @Singleton
     @Provides
     fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext app: Context) =
+        app.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE)
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPreferences: SharedPreferences) =
+        sharedPreferences.getString(KEY_NAME, "") ?: ""
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPreferences: SharedPreferences) =
+        sharedPreferences.getFloat(KEY_WEIGHT, 80f)
+
+    @Singleton
+    @Provides
+    fun provideFirstTimeToggle(sharedPreferences: SharedPreferences) =
+        sharedPreferences.getBoolean(KEY_FIRST_TIME_TOGGLE, true)
 }
 
 
--- app/src/main/java/com/gaura/learn/runtracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/gaura/learn/runtracker/ui/fragments/TrackingFragment.kt
@@ -29,6 +29,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 import java.lang.Math.round
 import java.util.*
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
@@ -40,7 +41,9 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     private var map: GoogleMap? = null
     private var curTimeInMillis = 0L
     private var menu: Menu? = null
-    private var weight = 80f
+
+    @set:Inject
+    var weight = 80f
 
     override fun onCreateView(
         inflater: LayoutInflater,
